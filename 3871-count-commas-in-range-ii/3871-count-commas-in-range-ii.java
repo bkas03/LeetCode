0a92ff62ed31bdffbfa612005b6c33b285cfa048
Bikas Kotal <bikaskotal03@gmail.com>
2026-09-09 09:38:31 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long threshold = 1000;
+
+        while (threshold <= n) {
+            ans += n - threshold + 1;
+
+            // Prevent overflow
+            if (threshold > n / 1000) {
+                break;
+            }
+
+            threshold *= 1000;
+        }
+
+        return ans;
+    }
+}
